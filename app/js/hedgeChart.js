9ae0040ff56ecffc98dc5be48360e3f97c91e338
vladbilenko <vlad.bilenko.se@gmail.com>
2018-06-14 17:18:57 +0300

    Finish landing (landing/index.html), add funds page (landing/funds.html), add assets page (landing/assets.html), add reviews page (landing/reviews.html), add article page (landing/cryptocurrency.html), add documents page (landing/documents.html), add about page (landing/about.html)

---
+++ app/js/hedgeChart.js
@@ -0,0 +1,21 @@
+// Highcharts.chart('container', {
+//     xAxis: {
+//         categories: ['7. May', '14. May', '21. May', '28. May'],
+//         labels: {
+//             reserveSpace: true
+//         },
+//         alignTicks: false,
+//     },
+//     yAxis: {
+//         title: false,
+//         labels: {
+//             format: '$ {value}'
+//         }
+//     },
+//
+//     series: [{
+//         data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3]
+//     }, {
+//         data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3]
+//     }]
+// });
